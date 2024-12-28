b99df9bb76e003a007929577df99f5387e4c67cc
SenDev2001 <senith2001.gamedev@gmail.com>
2024-12-28 23:04:48 +0000

    Add pause button

--- Velocity Rush 3D/Assets/Scripts/Core/M_GameManager.cs
+++ Velocity Rush 3D/Assets/Scripts/Core/M_GameManager.cs
@@ -5,24 +5,26 @@ using TMPro;
 public class M_GameManager : MonoBehaviour
 {
     public GameObject restartUI;
-    public TMP_Text scoreText;
-    public TMP_Text playerNameText;
+    public GameObject pauseButton;
+    
     private int score = 0;
-
     private string playerName;
+    
 
     void Start()
     {
         if (restartUI != null)
         {
             restartUI.SetActive(false);
+            
         }
 
-
+        if (pauseButton != null)
+        {
+            pauseButton.SetActive(true);
+        }    
         playerName = PlayerPrefs.GetString("PlayerName", "Guest");
 
-
-        UpdateScoreText();
     }
 
     public void ShowRestartUI()
@@ -33,18 +35,10 @@ public class M_GameManager : MonoBehaviour
             restartUI.SetActive(true);
             Time.timeScale = 0;
         }
-
-
-        if (playerNameText != null)
+        if (pauseButton != null)
         {
-            playerNameText.text = "Player: " + playerName;
+            pauseButton.SetActive(false);
         }
-
-        if (scoreText != null)
-        {
-            scoreText.text = "Score: " + score;
-        }
-
 
         M_ScoreManager scoreManager = FindObjectOfType<M_ScoreManager>();
         if (scoreManager != null)
@@ -70,16 +64,8 @@ public class M_GameManager : MonoBehaviour
     public void AddScore(int amount)
     {
         score += amount;
-        UpdateScoreText();
     }
 
-    private void UpdateScoreText()
-    {
-        if (scoreText != null)
-        {
-            scoreText.text = "Score: " + score;
-        }
-    }
 
     public void GoToMenu()
     {
---
+++ Velocity Rush 3D/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+ 
+
+}
