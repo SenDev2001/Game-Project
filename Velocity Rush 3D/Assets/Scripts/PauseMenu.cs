ba6eabc8e892682043accff31462d1594a0ece8f
SenDev2001 <senith2001.gamedev@gmail.com>
2024-12-30 22:57:48 +0000

    volume slider

--- Velocity Rush 3D/Assets/Scripts/PauseMenu.cs
+++ Velocity Rush 3D/Assets/Scripts/PauseMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject volumePanel;
     
     public void Pause()
     {
@@ -25,6 +26,17 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
- 
 
+    public void Volume()
+    {
+        pauseMenu.SetActive(false);
+        volumePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void CloseVolumePanel()
+    {
+        volumePanel.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
 }
